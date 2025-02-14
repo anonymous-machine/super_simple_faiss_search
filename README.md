83b246570d6155fca573df85691823004790136e
anonymous-machine <anonymous_github@wolber-wood.com>
2025-02-14 14:21:06 -0600

    Added README, major refactor
    run.py was refactored to better separate primary program functions; logging functionality was added; can now accept multiple paths

---
+++ README.md
@@ -0,0 +1,77 @@
+# Limitations
+
+This program does not have the ability to track moved files. If you move or
+rename a file, it will be indexed twice.
+
+There is not currently a way to remove an indexed path other than deleting the
+the ``sqlite.database`` and ``faiss.index`` files, and recreating the index.
+This is a limitation of the FAISS library.
+
+
+# Using
+
+1. Add the path you wish to index to config.ini
+
+2. Run run.py
+
+# Installation
+
+## Method 1: Using ``uv``
+
+I recommend using uv to create a virtual environment just for this program. Instructions are here.
+
+1. Install uv
+
+2. Navigate to the directory containing run.py
+
+3. Create the virtual env
+
+```bash
+uv venv create .
+```
+
+4. Install requirements
+
+```bash
+uv pip install -r requirements.txt
+```
+
+5. Edit config.ini to include the path to the files you wish to index. If you wish to index multiple paths, separate them with a ';'. E.g.,
+
+```
+Path to Files = C:/Users/John/Pictures;D:/OtherPictures
+```
+
+6. Run the program
+
+```bash
+uv run run.py 
+```
+
+## Method 2: Plain Python (no venv)
+
+1. Navigate to the firectory containing run.py
+
+2. Install requirements
+
+```bash
+pip install -r requirements.txt
+```
+
+or
+
+```bash
+pip3 install -r requirements.txt
+```
+
+3. Edit config.ini to include the path to the files you wish to index. If you wish to index multiple paths, separate them with a ';'. E.g.,
+
+```
+Path to Files = C:/Users/John/Pictures;D:/OtherPictures
+```
+
+4. Run the program
+
+```bash
+uv run run.py 
+```
--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import sqlite3
 import hashlib
 import subprocess
 import platform
+import logging
 from pathlib import Path
 
 from tqdm import tqdm
@@ -13,11 +14,26 @@ from PIL import Image
 import faiss
 
 def get_device() -> str:
+    logger.info("Getting ML device")
     if torch.cuda.is_available():
         return torch.device("cuda")
     elif torch.backends.mps.is_available():
         return torch.device("mps")
     return "cpu"
+
+def get_linux_image_viewer() -> str:
+    logger.info("Getting image viewer")
+    try:
+        viewer = subprocess.check_output(['xdg-mime', 'query', 'default', 'image/png']).decode('utf-8').strip()
+
+        if not viewer:
+            logger.error("No default image viewer found.")
+            return ''
+        logger.info(f'viewer is {viewer}')
+        return viewer
+    except subprocess.CalledProcessError as e:
+        logger.error(f"Error: {e}")
+        return ''
 
 def hash_string(string) -> str:
     hash_object = hashlib.sha256()
@@ -32,26 +48,53 @@ def to_absolute_path(path: Path) -> Path:
         return path.resolve()
     return path
 
-def open_file_list(file_list: list[Path] | list[str]) -> None:
-    print('opening files')
-    #print(file_list)
-    try:
-        match platform.system():
-            case "Darwin":
-                prefix = ['open']
-            case "Windows":
-                prefix = ['start']
-            case "Linux":
-                prefix = ['xdg-open']
-            case _:
-                print("I don't know your OS, sorry.")
-    except Exception as e:
-        print(f"Error opening files: {e}")
+def process_data_path(path: Path) -> bool:
+    data_dir = to_absolute_path(path)
+    if data_dir.exists() and not path.is_dir():
+        logger.error(f"Error: {path} exists but is not a directory.")
+        print(f"Error: {path} exists but is not a directory.")
+        return False
 
-    subprocess.run(prefix + file_list)
+    if not data_dir.exists():
+        logger.info(f"{path} does not exist, so it will be creates")
+        print(f"{path} does not exist, so it will be creates")
+        data_dir.mkdir(parents=True, exist_ok=True)
+
+def validate_image_path(path: Path) -> bool:
+    if not path.exists():
+        logger.error(f"Error: {path} does not exist")
+        print(f"Error: {path} does not exist")
+        return False
+    if not path.is_dir():
+        logger.error(f"Error: {path} is not a dir")
+        print(f"Error: {path} is not a dir")
+        return False
+    return True
+
+
+def open_file_list(file_list: list[Path] | list[str]) -> None:
+    logger.info('opening files')
+    logger.debug(f'file list is {file_list}')
+    user_platform = platform.system()
+    logger.info(f'user_platform is {user_platform}')
+
+    if user_platform == "Darwin":
+        subprocess.run(['open'] + file_list)
+        return
+    elif user_platform == "Windows":
+        subprocess.run(['start'] + file_list)
+        return
+    elif user_platform == "Linux":
+        image_viewer = get_linux_image_viewer()
+        if image_viewer == '':
+            print('Error: no image viewer found')
+            return
+        subprocess.run([image_viewer] + file_list)
 
 def load_clip_model(clip_model) -> (CLIPModel, AutoProcessor, AutoTokenizer):
+    logger.info('loading clip model')
     device = get_device()
+    logger.info(f'device is {device}')
     model = CLIPModel.from_pretrained(clip_model).to(device)
     processor = AutoProcessor.from_pretrained(clip_model)
     tokenizer = AutoTokenizer.from_pretrained(clip_model)
@@ -59,92 +102,89 @@ def load_clip_model(clip_model) -> (CLIPModel, AutoProcessor, AutoTokenizer):
     return model, processor, tokenizer
 
 def embed_image(file: Path, model: CLIPModel, processor: AutoProcessor) -> np.array:
+    logger.info(f'embedding image {file}')
     device = get_device()
+    logger.info(f'ML device is {device}')
     with Image.open(file) as img:
         inputs = processor(images=[img], return_tensors="pt").to(device)
         image_features = model.get_image_features(**inputs).cpu().detach().numpy().flatten()
+        logger.debug("image_features is {image_features}")
     return image_features
 
 def embed_text(text: str, model: CLIPModel, tokenizer: AutoTokenizer) -> np.array:
+    logger.info('embedding text {text}')
     device = get_device()
+    logger.info(f'ML device is {device}')
     inputs = tokenizer([text], padding=True,return_tensors="pt").to(device)
     text_features = model.get_text_features(**inputs).cpu().detach().numpy().flatten()
+    logger.debug(f'text_features is {text_features}')
     return text_features
 
-def file_search(root, cursor):
-    suffixes = {'.png', '.jpg', '.jpeg'} # I could make thie more resilient with mimetypes or magic. I'm probably not going to, so don't use weird image formats
-    image_files_on_disk = {str(f) for f in root.rglob("*") if f.suffix.lower() in suffixes}
-    image_files_in_database = {t[0] for t in cursor.execute("SELECT path FROM Files;").fetchall()}
+def file_search(path: Path, database_file: Path) -> list:
+    logger.info(f'running file search  on {path}')
+    suffixes = {'.png', '.jpg', '.jpeg'} # I could make this more resilient with mimetypes or magic. I'm probably not going to, so don't use weird image formats
+    image_files_on_disk = {str(f) for f in path.rglob("*") if f.suffix.lower() in suffixes}
+    logger.debug(f'there are {len(image_files_on_disk)} image files in the database')
+    image_files_in_database = set()
+
+    with sqlite3.connect(database_file) as conn:
+        logger.info('getting file list from database')
+        cursor = conn.cursor()
+        image_files_in_database = {t[0] for t in cursor.execute("SELECT path FROM Files;").fetchall()}
+        logger.debug('there are {len(image_files_in_database) files in the database')
+
     unindexed_files = image_files_on_disk - image_files_in_database
+    logger.info(f'there are {len(unindexed_files)} files on disk')
     return unindexed_files
 
-def main():
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    root = config["Storage"]["Path to Files"]
-    data_dir = config["Storage"]["Data Directory"]
-    clip_model = config["ML"]["CLIP Model"]
-    search_depth = int(config["ML"]["Search Depth"])
-
-    print('Checking path to images...')
-    root = to_absolute_path(Path(root))
-    if root.exists() and not root.is_dir():
-        print('Error: Path to Images should be a folder.')
-        return
-    if not root.exists():
-        print('Error: Path to Images does not appear to be correct.')
-        return
-
-    print('Checking Data Directory...')
-    data_dir = to_absolute_path(Path(data_dir))
-    if data_dir.exists() and not data_dir.is_dir():
-        print('Error: Data Directory should be a folder')
-        return
-    if not data_dir.exists():
-        print('Data Directory does not exist, so it will be created.')
-        data_dir.mkdir(parents=True, exist_ok=True)
-
-    model_hash = hash_string(clip_model)
-    data_dir = data_dir / Path(model_hash)
-    if not data_dir.exists():
-        data_dir.mkdir(parents=True,exist_ok=True)
-    sqlite_file = data_dir / Path('database.sqlite3')
-    index_file = data_dir / Path('faiss.index')
-    root = Path.cwd() #this should really be user configurable
-
-    # misc loading
-    model, processor, tokenizer = load_clip_model(clip_model=clip_model)
-    index = None
-    if index_file.exists():
-        index = faiss.read_index(str(index_file))
-
-    conn = sqlite3.connect(sqlite_file)
-    cursor = conn.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS Files (path text, vector_hash text);") 
-    conn.commit()
-
+def index_path(path: Path, database_file: Path, index_file: Path, model: CLIPModel, processor: AutoProcessor) -> None:
+    logger.info(f'Indexing {path}')
     new_files = set()
-    print('Bringing in new files...')
-    new_files = file_search(root=root, cursor=cursor)
-
     new_vectors = None
+    new_files = file_search(path=path, database_file=database_file)
+    create_new_index = False
+    logger.info('checking index file')
+    if index_file.exists():
+        logger.info('index_file exists, reading')
+        index = faiss.read_index(str(index_file))
+    else:
+        logger.info(f"index file does not exist, a new index will be created at {index_file}")
+        create_new_index = True
+
     count = 0
     for file in tqdm(new_files):
+        logger.info(f'processing file {file}')
         vector = embed_image(file=file, model=model, processor=processor)
         vector_hash = hash_vector(vector)
-        if index is None:
-            tqdm.write("Index file does not exist. A new index will be created")
-            d = vector.shape[0] 
+        if create_new_index:
+            logger.info('creating new index')
+            d = vector.shape[0]
+            logger.info(f'd is {d}')
             index = faiss.IndexFlatL2(d)
-        cursor.execute("INSERT INTO Files VALUES (?, ?)", (str(file), vector_hash,))
-        conn.commit()
+            create_new_index = False
+        with sqlite3.connect(database_file) as conn:
+            logger.info('inserting into database')
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO Files VALUES (?, ?)", (str(file), vector_hash, ))
+            conn.commit()
         if new_vectors is None:
+            logger.debug('creating new_vector matrix')
             new_vectors = np.zeros((len(new_files), vector.shape[0]))
         new_vectors[count] = vector
         count += 1
+
     if new_vectors is not None and len(new_vectors) > 0:
+        logger.info('Adding new vectors to index')
         index.add(new_vectors)
 
+    logger.info('Writing index to disk')
+    faiss.write_index(index, str(index_file))
+        
+
+def search_loop(database_file: Path, index_file: Path, model: CLIPModel, processor: AutoProcessor, tokenizer: AutoTokenizer, search_depth: int) -> None:
+    index = faiss.read_index(str(index_file))
+    conn = sqlite3.connect(database_file)
+    cursor = conn.cursor()
     while True:
         print('Type "N" to exit')
         search_string = input("Type a string that you want to search. If the thing you type looks like that path to an image, it will be used for a similarity search.\n")
@@ -166,12 +206,68 @@ def main():
         for r in results:
             print(f"r in results is {r}")
         file_list = [f[0][0] for f in results]
-        #print(file_list)
         open_file_list(file_list)
 
-    faiss.write_index(index, str(index_file))
-    conn.commit()
-    conn.close()
+        conn.close()
+
+
+def main():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    search_depth = int(config["ML"]["Search Depth"])
+    open_files = config.getboolean("ML", "Open Files")
+    logger.debug(f'search_depth is {search_depth}')
+    logger.debug(f'open_files is {open_files}')
+
+    index_paths = config["Storage"]["Path to Files"]
+    index_paths = index_paths.split(";")
+    index_paths = [to_absolute_path(Path(p)) for p in index_paths]
+    logger.debug(f'index_paths is {index_paths}')
+
+    clip_model = config["ML"]["CLIP Model"]
+    logger.debug(f'clip_model is {clip_model}')
+    model, processor, tokenizer = load_clip_model(clip_model=clip_model)
+    model_hash = hash_string(clip_model)
+
+    data_dir = config["Storage"]["Data Directory"]
+    data_dir = data_dir / Path(model_hash)
+    database_file = data_dir / Path('database.sqlite3')
+    index_file = data_dir / Path('faiss.index')
+    logger.debug(f'data_dir is {data_dir}')
+    logger.debug(f'database_file is {database_file}')
+    logger.debug(f'index_file is {index_file}')
+
+    logger.debug('validating image directories')
+    for path in index_paths:
+        logger.debug(f'validating {path}')
+        if not validate_image_path(path):
+            logger.error(f"{path} is invalid. Please edit config.ini to correct this error")
+            return
+
+    logger.info('checking data directory')
+    process_data_path(Path(data_dir))
+
+    with sqlite3.connect(database_file) as conn:
+        logger.info('creating Files tables')
+        cursor = conn.cursor()
+        cursor.execute("CREATE TABLE IF NOT EXISTS Files (path text, vector_hash text);") 
+        conn.commit()
+
+    for path in index_paths:
+        index_path(path, database_file=database_file, index_file=index_file, model=model, processor=processor)
+
+    search_loop(database_file=database_file, index_file=index_file, model=model, processor=processor, tokenizer=tokenizer, search_depth=search_depth)
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.StreamHandler(),
+        logging.FileHandler('app.log')
+    ]
+)
+
+logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
     main()
